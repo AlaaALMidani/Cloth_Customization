8ddeffbbd4cb9ae73cc392405a788a8b875aaa80
mohmmed <mohmmedalhassn@gmail.com>
2024-11-26 17:06:16 +0300

    finish mouse move

--- src/components/Couples.jsx
+++ src/components/Couples.jsx
@@ -4,7 +4,14 @@ import React from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF('/couples.glb')
+  const { nodes, materials ,scene } = useGLTF('/couples.glb')
+  scene.traverse((node) => {
+    if (node.isMesh) {
+      console.log('shado')
+      node.castShadow = true; // Enable shadow casting
+      node.receiveShadow = true; // Enable shadow receiving
+    }
+  });
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.cloth1.geometry} material={materials.initialShadingGroup} position={[3.059, 0, 0.125]} rotation={[Math.PI / 2, 0, 0]} scale={0.1} />
