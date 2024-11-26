23c4a3651db582d7dc2beabf6f6c57f6bd965213
Alaa Al-Midani <alaaalmedane@gmail.com>
2024-11-26 20:50:00 +0300

    nothing

--- src/components/Couples.jsx
+++ src/components/Couples.jsx
@@ -7,7 +7,10 @@ export function Model(props) {
   const { nodes, materials } = useGLTF('/couples.glb')
   return (
     <group {...props} dispose={null}>
-      <mesh geometry={nodes.cloth1.geometry} material={materials.initialShadingGroup} position={[3.059, 0, 0.125]} rotation={[Math.PI / 2, 0, 0]} scale={0.1} />
+      <mesh geometry={nodes.cloth1.geometry} 
+      material={materials.initialShadingGroup}
+       position={[3.059, 0, 0.125]}
+        rotation={[Math.PI / 2, 0, 0]} scale={0.1} />
     </group>
   )
 }
